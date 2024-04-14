84e7b942e8a31cfced57c8176b00f8c5c5ac1427
Lemar Andar <lemarandar@pm.me>
2024-04-13 20:03:50 -0400

    lifecycle nodes

--- build/introtask/CMakeFiles/introtask_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/introtask_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/introtask_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/more_interfaces/CMakeFiles/publish_address_book.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for publish_address_book.
-# This may be replaced when dependencies are built.
